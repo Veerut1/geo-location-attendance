0421b811ef8c7672c02cb07699593d3de6b908ea
veerut1 <veeru.tippa@live.in>
2026-08-10 16:57:09 +0530

    Removed data

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,1 @@
-window.ATTENDANCE_USE_FIRESTORE = true;
 
-window.ATTENDANCE_FIREBASE_CONFIG = {
-  "apiKey": "AIzaSyCg7mVLGveR_02QjthaCGS42zjdbol92JI",
-  "authDomain": "geo-location-attendance-b4efb.firebaseapp.com",
-  "projectId": "geo-location-attendance-b4efb",
-  "storageBucket": "geo-location-attendance-b4efb.firebasestorage.app",
-  "messagingSenderId": "191621800728",
-  "appId": "1:191621800728:web:de8af19448ae0ab4fd346d"
-};
-
-window.ATTENDANCE_ADMIN_PASSCODE = "admin@123";
